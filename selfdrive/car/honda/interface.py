3d94408a5f76f621e881c0e51f12ab4cdb7c0128
cfranhonda <46506059+cfranhonda@users.noreply.github.com>
2020-10-06 22:58:09 -0700

    0.00007

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -147,7 +147,7 @@ class CarInterface(CarInterfaceBase):
     # For modeling details, see p.198-200 in "The Science of Vehicle Dynamics (2014), M. Guiggiani"
     ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0], [0]]
     ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0.], [0.]]
-    ret.lateralTuning.pid.kf = 0.00006  # conservative feed-forward
+    ret.lateralTuning.pid.kf = 0.00007  # conservative feed-forward
 
     eps_modified = True
     for fw in car_fw:
